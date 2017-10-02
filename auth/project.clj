f6b8db5416d3d35ab13714bf83e176fc36f69efb
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-10-02 12:57:15 +0200

    Initial setup with Clojure and a database in a compose env

---
+++ auth/project.clj
@@ -0,0 +1,9 @@
+(defproject auth "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url  "https://choosealicense.com/licenses/mit/"}
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
+  :main ^:skip-aot auth.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ db/README.md
@@ -0,0 +1,1 @@
+# I don't do a whole lot yet
