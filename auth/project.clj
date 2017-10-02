7164681052828751d863267496195c3e7c754e1c
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-10-02 14:49:53 +0200

    Add database config for jdbc

--- auth/project.clj
+++ auth/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url  "https://choosealicense.com/licenses/mit/"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [environ "1.1.0"]
+                 [org.clojure/java.jdbc "0.7.1"]]
   :main ^:skip-aot auth.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- auth/src/auth/core.clj
+++ auth/src/auth/core.clj
@@ -1,5 +1,14 @@
 (ns auth.core
-  (:gen-class))
+  (:gen-class)
+  (:require [environ.core :refer [env]]))
+
+(def db {:dbtype "postgresql"
+         :dbname (env :DATABASE_NAME)
+         :host "db"
+         :user (env :DATABASE_USER)
+         :password (env :DATABASE_PASS)
+         :ssl true
+         :sslfactory "org.postgresql.ssl.NonValidatingFactory"})
 
 (defn -main
   "I don't do a whole lot ... yet."
