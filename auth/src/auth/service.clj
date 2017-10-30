61de75f400f6a41561da9853fd19f04ed8bb9a6e
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-10-30 11:22:08 +0100

    Store new users to database

--- auth/src/auth/service.clj
+++ auth/src/auth/service.clj
@@ -3,12 +3,13 @@
             [io.pedestal.http :as http]
             [io.pedestal.http.body-params :as body-params]
             [ring.util.response :as ring-resp]
+            [korma.core :as kc]
             [korma.db :as kdb]
             [clojure.data.json :as json]
             [clj-http.client :as client]
             [hiccup.page :as hp]))
 
-(def db {:dbtype "postgresql"
+#_(def db {:dbtype "postgresql"
          :dbname (System/getenv "DATABASE_NAME")
          :host "db"
          :user (System/getenv "DATABASE_USER")
@@ -24,6 +25,11 @@
                  :host "db"
                  :port "5432"
                  :delimiters ""}))
+
+;; nickname | recipient_service | recipient_id | sender_id | timestamp
+;; Chris    | Facebook          | 4242123      | 12310238  | 1282136
+
+(def users_auth (atom {}))
 
 ;; -----------------------------------------------------------------------------
 
@@ -50,19 +56,36 @@
     (try
       (client/post "https://dbas.cs.uni-duesseldorf.de/api/login"
                    {:body (json/write-str {:nickname account :password password})})
+      (println "Login successful. Redirecting to" redirect)
+      (swap! users_auth assoc uuid account)
       (ring-resp/redirect redirect)
       (catch Exception _
+        (println "Login not successful. Redirecting to" redirect_uri)
         (ring-resp/redirect redirect_uri)))))
 
-(defn success-page [{{:keys [recipient sender account_linking]} :json-params :as params}]
-  (println "\n\n\n\n\n\n\nSUCCESS ###################")
-  (println {:params params})
-  (ring-resp/response (hp/html5 [:h1 "Success!"])))
-
-;; {:json-params {:recipient {:id 1144092719067446}, :timestamp 1509116849482, :sender {:id 1417200965011924}, :account_linking {:authorization_code f9db521f-574d-4df4-bc62-d1c6ba5abeae, :status linked}}
-
 (s/def ::id string?)
-(s/def ::messaging coll?)
+(s/def ::recipient (s/keys :req-un [::id]))
+(s/def ::timestamp pos-int?)
+(s/def ::sender (s/keys :req-un [::id]))
+(s/def ::authorization_code string?)
+(s/def ::status string?)
+(s/def ::account_linking (s/keys :req-un [::status]
+                                 :opt-un [::authorization_code]))
+(s/def ::success-params
+  (s/keys :req-un [::recipient ::timestamp ::sender ::account_linking]))
+
+(defn success-page [{{:keys [recipient sender account_linking]} :json-params :as params}]
+  (println "\nSUCCESS ###################")
+  (println {:params params})
+  (when (s/valid? ::success-params (:json-params params))
+    (let [auth (:authorization_code account_linking)
+          nickname (get @users_auth auth)]
+      (kc/insert users (kc/values {:nickname nickname :service "Facebook" :app_id (:id recipient) :user_id (:id sender)}))
+      (swap! users_auth dissoc auth))
+    (ring-resp/response (hp/html5 [:h1 "Success!"]))))
+
+;; {:recipient {:id 1144092719067446}, :timestamp 1509354888386, :sender {:id 1417200965011924},
+;;  :account_linking {:authorization_code eaeb9728-5f99-48ba-816d-9f08917c5b99, :status linked}}
 
 ;; -----------------------------------------------------------------------------
 
