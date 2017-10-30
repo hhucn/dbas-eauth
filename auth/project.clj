b3012d52f34d44e75ddc5118d27102d37f055e9b
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-10-30 10:33:32 +0100

    Add test.check dep

--- auth/project.clj
+++ auth/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+                 [org.clojure/test.check "0.9.0"]
                  [io.pedestal/pedestal.service "0.5.3"]
                  [io.pedestal/pedestal.jetty "0.5.3"]
 
