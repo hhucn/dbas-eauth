7605f198c7d8223e85a6199ef3d7548cdd57339f
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-11-10 13:43:25 +0100

    Bump clj version

--- auth/project.clj
+++ auth/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [org.clojure/test.check "0.9.0"]
                  [io.pedestal/pedestal.service "0.5.3"]
                  [io.pedestal/pedestal.jetty "0.5.3"]
